b6f5d80ce19c33cb482ec25f00a9554d6b488f6a
nina de jesus <satifice@riseup.net>
2015-12-02 09:22:49 -0500

    start fresh for the day

--- batch-create-dspace-packets.sh
+++ batch-create-dspace-packets.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
-sed ':a;N;$!ba;s/\n/ /g'
+#sudo python xlsx2csv/xlsx2csv.py -e -d "|" xls_sample.xlsx sample.csv #don't forget to swap these out for CLI args $1 and $2
 
-tr '\n' ' ' < input_filename
+field_number=$(head -n 1 sample.csv | awk -F'|' '{print NF-1}')
+field_headers=$(head -n 1 sample.csv)
+objects='test-files/'
+
+#echo $field_headers
+#echo $no_headers
+for i in $(ls $objects)
+do
+    COUNTER=1
+    while [ $COUNTER -le $field_number ]
+    do
+        cut -d'|' -f$COUNTER sample.csv | tail -n +2
+        #echo $COUNTER
+        COUNTER=$((COUNTER+1))
+    done
+done
+#sed ':a;N;$!ba;s/\n/ /g'
+
+#tr '\n' ' ' < input_filename
