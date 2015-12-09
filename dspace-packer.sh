f1380648a4c7fb9cad8ad98dfee91a4f444115e1
nina de jesus <satifice@riseup.net>
2015-12-09 15:43:24 -0500

    trying to figure out how to get the newlines into the resulting dublin core record

--- dspace-packer.sh
+++ dspace-packer.sh
@@ -2,10 +2,10 @@
 
 #sudo python xlsx2csv/xlsx2csv.py -e -d "|" xls_sample.xlsx sample.csv #don't forget to swap these out for CLI args $1 and $2
 
-csv=sample2.csv
+csv='sample2.csv'
 field_number=$(head -n 1 $csv | awk -F'|' '{print NF-1}')
 field_headers=$(head -n 1 $csv)
-objects='test-files/'
+objects='test-files'
 no_objects=$(ls -1 $objects | wc -l)
 c1=1
 c2=1
@@ -35,13 +35,13 @@ while read $field_headers
 do
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" > record.$identifier/dublin_core.xml
     echo "<dublin_core>" >> record.$identifier/dublin_core.xml
-    echo "  <dcvalue qualifier=\"none\" element=\"title\">$title</dcvalue>" >> record.$identifier/dublin_core.xml
+    echo -e "  <dcvalue qualifier=\"none\" element=\"title\">$title</dcvalue>" >> record.$identifier/dublin_core.xml
     echo "  <dcvalue qualifier=\"author\" element=\"contributor\">$author</dcvalue>" >> record.$identifier/dublin_core.xml
     echo "  <dcvalue element=\"contributor\" qualifier=\"advisor\">$advisor</dcvalue>" >> record.$identifier/dublin_core.xml
     echo "  <dcvalue qualifier=\"issued\" element=\"date\">$year</dcvalue>" >> record.$identifier/dublin_core.xml
     echo "  <dcvalue qualifier=\"none\" element=\"identifier\">$identifier</dcvalue>" >> record.$identifier/dublin_core.xml
     echo "  <dcvalue qualifier=\"citation\" element=\"identifier\">$citation</dcvalue>" >> record.$identifier/dublin_core.xml
-    echo "  <dcvalue qualifier=\"abstract\" element=\"description\">$abstract</dcvalue>" >> record.$identifier/dublin_core.xml
+    echo $abstract #"  <dcvalue qualifier=\"abstract\" element=\"description\">$abstract</dcvalue>" #>> record.$identifier/dublin_core.xml
     echo "  <dcvalue qualifier=\"none\" element=\"type\">$type</dcvalue>" >> record.$identifier/dublin_core.xml
     echo "</dublin_core>" >> record.$identifier/dublin_core.xml
 
@@ -51,7 +51,7 @@ done
 IFS=$OLDIFS
 }  
 
-package
+#package
 populate
 
 
