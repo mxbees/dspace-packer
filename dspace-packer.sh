9bae253b0b8287c4ab0e12e3eb72c05fe5c7c404
nina de jesus <satifice@riseup.net>
2015-12-10 14:53:20 -0500

    ensures that the dc record has the newlines unescaped and all ampersands are changed into character entities

--- dspace-packer.sh
+++ dspace-packer.sh
@@ -12,10 +12,10 @@ c2=1
 c3=1
 id=$( ls $objects | xargs -I{} basename {} .pdf )
 #mkdir /tmp/dspace
-sed 1,1d $csv > /tmp/dspace/no_head.csv
+sed 1,1d $csv > /tmp/dspace/no_headers.csv
 
 #make packages
-package () {
+make_simple_archive_format_package () {
 ls $objects > /tmp/dspace/objects.txt
 while read line
 do
@@ -26,32 +26,37 @@ do
 done < /tmp/dspace/objects.txt
 }
 
-populate () {
+make_dc_record () {
 OLDIFS=$IFS
 IFS='|'
 for j in $( ls $objects ) 
 do
-while read $field_headers
-do
-    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" > record.$identifier/dublin_core.xml
-    echo "<dublin_core>" >> record.$identifier/dublin_core.xml
-    echo -e "  <dcvalue qualifier=\"none\" element=\"title\">$title</dcvalue>" >> record.$identifier/dublin_core.xml
-    echo "  <dcvalue qualifier=\"author\" element=\"contributor\">$author</dcvalue>" >> record.$identifier/dublin_core.xml
-    echo "  <dcvalue element=\"contributor\" qualifier=\"advisor\">$advisor</dcvalue>" >> record.$identifier/dublin_core.xml
-    echo "  <dcvalue qualifier=\"issued\" element=\"date\">$year</dcvalue>" >> record.$identifier/dublin_core.xml
-    echo "  <dcvalue qualifier=\"none\" element=\"identifier\">$identifier</dcvalue>" >> record.$identifier/dublin_core.xml
-    echo "  <dcvalue qualifier=\"citation\" element=\"identifier\">$citation</dcvalue>" >> record.$identifier/dublin_core.xml
-    echo $abstract #"  <dcvalue qualifier=\"abstract\" element=\"description\">$abstract</dcvalue>" #>> record.$identifier/dublin_core.xml
-    echo "  <dcvalue qualifier=\"none\" element=\"type\">$type</dcvalue>" >> record.$identifier/dublin_core.xml
-    echo "</dublin_core>" >> record.$identifier/dublin_core.xml
-
-
-done < /tmp/dspace/no_head.csv
+    while read -r $field_headers
+    do
+        echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" > record.$identifier/dublin_core.xml
+        echo "<dublin_core>" >> record.$identifier/dublin_core.xml
+        printf '%b\n' "  <dcvalue qualifier=\"none\" element=\"title\">$title</dcvalue>" >> record.$identifier/dublin_core.xml
+        printf '%b\n' "  <dcvalue qualifier=\"author\" element=\"contributor\">$author</dcvalue>" >> record.$identifier/dublin_core.xml
+        printf '%b\n' "  <dcvalue element=\"contributor\" qualifier=\"advisor\">$advisor</dcvalue>" >> record.$identifier/dublin_core.xml
+        printf '%b\n' "  <dcvalue qualifier=\"issued\" element=\"date\">$year</dcvalue>" >> record.$identifier/dublin_core.xml
+        printf '%b\n' "  <dcvalue qualifier=\"none\" element=\"identifier\">$identifier</dcvalue>" >> record.$identifier/dublin_core.xml
+        printf '%b\n' "  <dcvalue qualifier=\"citation\" element=\"identifier\">$citation</dcvalue>" >> record.$identifier/dublin_core.xml
+        printf '%b\n' "  <dcvalue qualifier=\"abstract\" element=\"description\">$abstract</dcvalue>" >> record.$identifier/dublin_core.xml
+        printf '%b\n' "  <dcvalue qualifier=\"none\" element=\"type\">$type</dcvalue>" >> record.$identifier/dublin_core.xml
+        echo "</dublin_core>" >> record.$identifier/dublin_core.xml
+    done < /tmp/dspace/no_head.csv
 done
 IFS=$OLDIFS
-}  
+}
 
-#package
-populate
+clean_ampersands () {
+find record.* -name dublin_core.xml > /tmp/dspace/dc_records.txt
+while read line
+do
+    sed -i 's/&/&amp;/g' $line
+done < /tmp/dspace/dc_records.txt
+}
 
-
+#make_simple_archive_format_package
+make_dc_record
+clean_ampersands
